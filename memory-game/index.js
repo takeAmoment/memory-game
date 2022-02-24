919a862f62e3de2724667fd4b2e0a8e2d2ce39c6
alpekhota <alekspekhota@gmail.com>
2022-02-24 20:55:40 +0100

    feat: initialize constants and create function flipCard

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,8 +1,21 @@
 let cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard(){
-    console.log('flip')
+    
     this.classList.toggle('flip');
+
+    if(!hasFlippedCard){
+
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+    }
 }
