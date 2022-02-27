2bc132edc7a01f22ad4ae48e236c0524764bdad5
alpekhota <alekspekhota@gmail.com>
2022-02-27 17:43:23 +0100

    feat: add styles for footer

--- memory-game/index.js
+++ memory-game/index.js
@@ -3,19 +3,61 @@ let cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let firstCard;
 let secondCard;
+let lockCard = false;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard(){
-    
-    this.classList.toggle('flip');
+    if(lockCard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
 
     if(!hasFlippedCard){
 
         hasFlippedCard = true;
         firstCard = this;
     } else {
-        hasFlippedCard = false;
+    
         secondCard = this;
+        checkMatches();
+    }
+    console.log(firstCard, secondCard);
+
+    
+    
+}
+
+function checkMatches(){
+    if(firstCard.dataset.image === secondCard.dataset.image){
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+
+        resetBoard()
+    }else{
+        lockCard = true;
+        setTimeout(()=> {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            resetBoard()
+            
+        }, 1500);
+        
     }
 }
+
+function resetBoard(){
+    [hasFlippedCard, lockCard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function shuffleCards(){
+    
+
+    cards.forEach(card => {
+        let random = Math.floor(Math.random()*cards.length);
+        card.style.order = random;
+    });
+}
+
+window.addEventListener('load', shuffleCards);
