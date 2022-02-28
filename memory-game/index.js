954b6b07d78a4af069c3224a02c17f48d4d6b95d
alpekhota <alekspekhota@gmail.com>
2022-02-28 11:42:11 +0100

    feat: creation appearance modalWindow and start game again after that

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,9 +1,15 @@
 let cards = document.querySelectorAll('.memory-card');
 
+let modalWindow = document.querySelector('.window');
+let text = document.querySelector('.text');
+let closeBtn = document.querySelector('.start');
+
 let hasFlippedCard = false;
 let firstCard;
 let secondCard;
 let lockCard = false;
+let step = 0;
+let flippedCard = 10;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
@@ -32,7 +38,7 @@ function checkMatches(){
     if(firstCard.dataset.image === secondCard.dataset.image){
         firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard);
-
+        flippedCard++;
         resetBoard()
     }else{
         lockCard = true;
@@ -44,6 +50,8 @@ function checkMatches(){
         }, 1500);
         
     }
+    step++;
+    console.log(step);
 }
 
 function resetBoard(){
@@ -61,3 +69,25 @@ function shuffleCards(){
 }
 
 window.addEventListener('load', shuffleCards);
+
+/*------------------------------------------------*/
+
+if(flippedCard === 10){
+    text.innerHTML = `You open all cards in ${step} steps`
+    modalWindow.classList.add('open');
+}
+
+function removeFlip(){
+    cards.forEach(card => card.classList.remove('flip'));
+    shuffleCards();
+    flippedCard = 0;
+    step = 0;
+    hasFlippedCard = false;
+    lockCard = false;
+}
+function closeModalWindow(){
+    modalWindow.classList.remove('open');
+    removeFlip();
+}
+
+closeBtn.addEventListener('click', closeModalWindow);
